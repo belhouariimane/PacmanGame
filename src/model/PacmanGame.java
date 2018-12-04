f23b8f2062f8077673c3875c95a6f75387b80611
belhouariimane <41473840+belhouariimane@users.noreply.github.com>
2018-12-04 16:15:28 +0100

    afficher labyrinthe

--- src/model/PacmanGame.java
+++ src/model/PacmanGame.java
@@ -2,6 +2,18 @@ package model;
 
 
 public class PacmanGame extends Game {
+	Maze maze;
+	public Maze getMaze() {
+		return maze;
+	}
+
+
+
+	public void setMaze(Maze maze) {
+		this.maze = maze;
+	}
+
+
 
 	public PacmanGame(int maxTours) {
 		super(maxTours);
--- src/test/testGame.java
+++ src/test/testGame.java
@@ -2,6 +2,8 @@ package test;
 import controller.AdvancedController;
 import controller.BasicController;
 import controller.Controller;
+import model.Game;
+import model.PacmanGame;
 import model.SimpleGame;
 import view.ViewCommande;
 import view.ViewGame;
@@ -14,8 +16,9 @@ public class testGame {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SimpleGame game=new SimpleGame(40);
-		Controller controller_game=new AdvancedController(game);
+		Game g=new PacmanGame(100);
+		Controller controller_game=new AdvancedController(g);
+		ViewGame.choosePacmanLayout(g); 
 	}
 
 }
